831d628581369b439c5f497c0fbf49eb95d7db8a
Nnnathanlethaii <nathanlethai8@gmail.com>
2025-05-03 21:09:42 -0400

    implement script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+function gdpCalc(c,i,g,nx){
+        const gdp = c+i+g+nx;
+        console.log(`Total GDP: $(gdp)`)
+}
+
+const c = 10
+const i = 23
+const g = 120
+const nx = -12
+
+gdpCalc(c,i,g,nx); 
